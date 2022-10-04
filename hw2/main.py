9629dc7ad76a683944c8f89ec3b09cf593625b05
Руслан Русланов <sharp_blade666@mail.ru>
2022-10-04 17:20:22 +0300

    hw2 done

--- hw2/main.py
+++ hw2/main.py
@@ -1,14 +1,19 @@
-import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+import numpy as np
+
+
+def saveplot(path):
+    plt.tight_layout()
+    plt.savefig('results/' + path)
+
 
 if __name__ == '__main__':
     N = 100  # number of samples
     time = np.arange(0, N, 1)
     muPsi, sigmaPsi = 0, 1  # mean and standard deviation that describe the motion model error
-    muEta, sigmaEta = 0, 50  # mean and standard deviation that describe the sensor error
+    muEta, sigmaEta = 0, 50  # mean and standard deviation that describe the sensor error - у сенсора больше разброс
     errorPsi = np.random.normal(muPsi, sigmaPsi, 100)  # pdf of the motion model error
-    errorEta = np.random.normal(muEta, sigmaEta, 100)  # pdf of the sensor error
+    errorEta = np.random.normal(muEta, sigmaEta, 100)  # pdf of the sensor error - данные сенсора
 
     plt.title('Motion model and Sensor Variances', fontsize=15)
     plt.xlabel('time', fontsize=12)
@@ -20,15 +25,16 @@ if __name__ == '__main__':
 
     # Creation of the toycar's GPS data
 
-    a = 0.1  # acceleration
+    a = 0.1  # ускорение
+    # нулевые массивы значения координат
     x = np.zeros(N + 1)
     z = np.zeros(N + 1)
 
-    time = np.arange(0, N + 1, 1)
+    time = np.arange(0, N + 1, 1)  # ось Х времени
 
     for t in range(0, N):
-        x[t + 1] = x[t] + a * t + np.random.normal(muPsi, sigmaPsi, 1)
-        z[t + 1] = x[t + 1] + np.random.normal(muEta, sigmaEta, 1)
+        x[t + 1] = x[t] + a * t + np.random.normal(muPsi, sigmaPsi, 1)  # данные модели движения
+        z[t + 1] = x[t + 1] + np.random.normal(muEta, sigmaEta, 1)  # имитация данных сенсора
 
     plt.title('Original Car Motion & Noisy Sensor data', fontsize=15)
     plt.xlabel('time', fontsize=12)
@@ -48,6 +54,7 @@ if __name__ == '__main__':
         eOpt[t + 1] = np.sqrt(
             (sigmaEta ** 2) * (eOpt[t] ** 2 + sigmaPsi ** 2) / (sigmaEta ** 2 + eOpt[t] ** 2 + sigmaPsi ** 2))
         K[t + 1] = (eOpt[t + 1] ** 2) / (sigmaEta ** 2)
+        # и в итоге - отфитрованные значения сенсора
         xOpt[t + 1] = (xOpt[t] + a * t) * (1 - K[t + 1]) + K[t + 1] * z[t + 1]
 
     plt.subplot(1, 2, 1)
@@ -69,6 +76,23 @@ if __name__ == '__main__':
     plt.xlabel('time', fontsize=12)
     plt.ylabel('K', fontsize=12)
     plt.plot(time, K, 'g', label='Kalman Gain')
-    #plt.subplots_adjust(bottom=0.1, right=2, top=0.9)
+    # plt.subplots_adjust(bottom=0.1, right=2, top=0.9)
     plt.legend()
     plt.show()
+
+    # Home work 2: Create simplified recursive filter and compare its results with 1D Kalman filter
+    xRec = np.zeros(N + 1)
+    for k in range(1, N + 1):
+        xRec[k] = (1 - K[k]) * xRec[k - 1] + K[k] * z[k]
+
+    plt.title('Comparing Recursive and Kalman Filtering', fontsize=15)
+    plt.xlabel('time', fontsize=12)
+    plt.ylabel('x coordinate', fontsize=12)
+    plt.plot(time, x, label='original coordinate')
+    plt.plot(time, z, 'g', label='noisy sensor data')
+    plt.plot(time, xOpt, 'r', label='Kalman Filter output')
+    plt.plot(time, xRec, 'orange', label='Recursive Filter output')
+    saveplot('compare_recursive_and_kalman.png')
+    plt.legend()
+    plt.show()
+    # вывод: сенсорным данным слабо доверяем, так как коэффициент K(Kalman Gain) из-за зашумленности близок к 0
