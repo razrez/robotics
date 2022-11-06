8596994eb9c4b74220b48bb705f73825e76279d8
Руслан Русланов <sharp_blade666@mail.ru>
2022-11-06 17:52:47 +0300

    imaging grid of pictures in RGB with plt

--- hw7/main.py
+++ hw7/main.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import cv2 as cv2
-
+import numpy as np
 
 if __name__ == '__main__':
     pic = cv2.imread('spiral_galaxy.jpg')
@@ -24,10 +24,17 @@ if __name__ == '__main__':
     for h in range(h_interval-1, height-1, h_interval):
         w0 = 1
         for w in range(w_interval-1, width-1, w_interval):
-            print(h0,h,w0,w)
+            #print(h0,h,w0,w)
             cropped_pic = img[h0:h, w0:w]
             cv2.imwrite(f"cropped_picture/Cropped Image{picCounter}.jpg", cropped_pic)
             w0 = w
             picCounter += 1
         h0 = h
 
+    # show cropped pictures in RGB using matplotlib!!
+    f, axarr = plt.subplots(3, 3)
+    for i in range(1, picCounter):
+        pic = cv2.imread(f'cropped_picture/Cropped Image{i}.jpg')
+        axarr[i // 3 - 1, i % 3 - 1].imshow(cv2.cvtColor(pic, cv2.COLOR_BGR2RGB))
+        axarr[i // 3 - 1, i % 3 - 1].axis('off')
+    plt.show()
